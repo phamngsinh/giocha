fb7cc78b7196e1200f2e524d4382a63e881e46de
ken.vu@qsoftvietnam.com <ken.vu@qsoftvietnam.com>
2016-05-05 11:38:23 +0700

    update

--- app/view/order/List.js
+++ app/view/order/List.js
@@ -14,30 +14,28 @@ Ext.define('TutorialApp.view.order.List', {
     },
     columns: [
         {text: 'ID', dataIndex: 'id', flex: 1},
-        {text: 'Note', dataIndex: 'note', flex: 1},
-        {text: 'Status', dataIndex: 'status', flex: 1},
-        {text: 'Customer', dataIndex: 'user_id', flex: 1},
-        {text: 'Product', dataIndex: 'product_id', flex: 1},
-        {text: 'Quantity', dataIndex: 'quantity', flex: 1}
+        {text: 'Tên khách hàng', dataIndex: 'user_id', flex: 1},
+        {text: 'Sản phẩm', dataIndex: 'product_id', flex: 1},
+        {text: 'Thanh toán', dataIndex: 'quantity', flex: 1}
     ],
     dockedItems: [
-    {
-        xtype: 'toolbar',
-        flex: 1,
-        dock: 'bottom',
-        items: [
-            {
-                xtype: 'button',
-                text: 'Add',
-                itemId: 'add',
-                iconCls: 'x-fa fa-plus',
-                handler: function() {
-                    var addModal = Ext.create('TutorialApp.view.order.Datbanh');
-                    addModal.show();
+        {
+            xtype: 'toolbar',
+            flex: 1,
+            dock: 'bottom',
+            items: [
+                {
+                    xtype: 'button',
+                    text: 'Add',
+                    itemId: 'add',
+                    iconCls: 'x-fa fa-plus',
+                    handler: function () {
+                        var addModal = Ext.create('TutorialApp.view.order.Datbanh');
+                        addModal.show();
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        }
     ],
     listeners: {
         select: 'onItemSelected'
